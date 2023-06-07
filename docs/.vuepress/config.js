6f5fd3f44d508667113b21dd573463accd5716b3
403NotFound <1214972923@qq.com>
2023-06-07 22:13:41 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: "杂乱有序",
+  description: "Just playing around",
+  base: "/docs",
+  themeConfig: {
+    sidebar: "auto",
+    nav: [
+      { text: "HTML", link: "/FE/html" },
+      { text: "CSS", link: "/FE/css" },
+      { text: "Javascript", link: "/FE/javascript" },
+      { text: "Vue", link: "/FE/vue" },
+      { text: "React", link: "/FE/react" },
+      { text: "Webpack", link: "/FE/webpack" },
+      { text: "工程化", link: "/FE/project" },
+      { text: "手撕代码", link: "/FE/hand" },
+    ],
+  },
+};
